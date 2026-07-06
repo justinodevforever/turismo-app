9fde627a1afde49590e0cb2862ff47202621a0d7
Justino Chinilila Chitombi <jchitombi7@email.com>
2026-07-06 14:04:50 +0100

    foi adionado no arquivo build.sh script para criar uper user 06-07-2026

--- build.sh
+++ build.sh
@@ -5,3 +5,20 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superusuário criado.")
+else:
+    print("Superusuário já existe ou variáveis não configuradas.")
+EOF
