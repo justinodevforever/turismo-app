0169cf4385b5790ab4ad77f450ca905273273a07
Justino Chinilila Chitombi <jchitombi7@email.com>
2026-07-15 12:48:27 +0100

    15-07-2026 atualizado

--- config/settings.py
+++ config/settings.py
@@ -117,21 +117,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # ============================================================
 # BANCO DE DADOS — PostgreSQL + PostGIS
 # ============================================================
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
-}
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('NAME_DB'),
-#         'USER': config('USER_DB'),
-#         'PASSWORD': config('PASSWORD_DB'),
-#         'HOST': config('HOST_DB'),
-#         'PORT': config('PORT_DB'),
-#     }
+#     "default": dj_database_url.config(
+#         default=os.environ.get("DATABASE_URL")
+#     )
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME_DB'),
+        'USER': config('USER_DB'),
+        'PASSWORD': config('PASSWORD_DB'),
+        'HOST': config('HOST_DB'),
+        'PORT': config('PORT_DB'),
+    }
+}
 
 TEMPLATES = [
     {
